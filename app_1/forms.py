e3fd0e5d994305a3f52d83cc00d672c5cc3ac3a8
EsteTruji <82886890+EsteTruji@users.noreply.github.com>
2022-09-09 00:09:52 -0500

    Delete app_1 directory

--- app_1/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-#from .models import etiqueta
-
-class UploadImageForm(forms.ModelForm):
-    
-    class Meta:
-        #model = etiqueta
-        fields = ['carpeta', 'img']
