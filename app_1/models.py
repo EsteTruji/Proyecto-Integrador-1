12d59aed8bf54aebc193965f8bb220cec4636b48
EsteTruji <etrujilloc@eafit.edu.co>
2022-09-09 00:07:18 -0500

    Cambios estéticos (uso de Bootstrap) y estructuración y creación completa de la base de datos

--- app_1/models.py
+++ app_1/models.py
@@ -1,21 +1,32 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
-class Usuarios(models.Model):
-    codigo = models.CharField(max_length=6)
-    nombre = models.CharField(max_length=45)
-    cantidadRecogida = models.IntegerField(default=0)
+class Usuario(models.Model):
+	id_usuario = models.AutoField(primary_key=True, blank=False)
+	nombre = models.CharField(max_length=45, null=False)
 
-class Canecas(models.Model):
-    caneca_id = models.CharField(max_length=6)
-    sector = models.CharField(max_length=20)
-    cantidadRecogida = models.IntegerField(default=0)
-    tipo = models.CharField(max_length=10)
+class Etiqueta(models.Model):
+	id_archivo = models.PositiveBigIntegerField(primary_key=True, blank=False)
+	Usuario_id_usuario = models.ForeignKey(Usuario, null= False, blank=False, on_delete=models.CASCADE)
+	etiqueta = models.JSONField(null=False, blank=False)
+	caneca = models.CharField(max_length=45, null=False, blank=False, choices=((0,'Ordinario'), (1,'Papel_Carton'), (2,'Plastico'), (3,'Vidrio'), (4,'Organicos'), (5,'Residuos_Peligrosos'), (6,'Aluminio')))
+	fecha_etiquetado = models.DateTimeField(auto_now_add=True)
 
-class Incentivos(models.Model):
-	codigo = models.CharField(max_length=6)
-	puntos = models.IntegerField(default=0)
+class Residuo(models.Model):
+	Etiqueta_id_archivo = models.ForeignKey(Etiqueta, null=True, blank=False, on_delete=models.CASCADE)
+	id_residuo = models.PositiveIntegerField(blank=False)
+	
+class Puntos(models.Model):
+	Usuario_id_usuario = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
+	cantidad_puntos = models.PositiveIntegerField(default=0)
+	fecha_puntos = models.DateTimeField(auto_now_add=True)
+
+class Actividad(models.Model):
+	Usuario_id_usuario = models.ForeignKey(Usuario, null= False, blank=False, on_delete=models.CASCADE)
+	tipo_actividad = models.CharField(max_length=45, null=False, blank=False, choices=((0,'etiquetado'),(1,'clasificado')))
+	fecha_actividad = models.DateTimeField(auto_now_add=True)
 
 #python manage.py makemigrations
 #python manage.py sqlmigrate app_1 0001
--- app_1/views.py
+++ app_1/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app_1.models import Incentivos, Usuarios
+from app_1.models import Puntos, Usuario
 
 id2 = 1
 
 def home(request):
-    usuarios_obj = Usuarios.objects.get(id = id2)
+    usuarios_obj = Usuario.objects.get(id_usuario = id2)
     return render(request, 'home.html', {'nombre':usuarios_obj.nombre})
 
 def mis_incentivos(request):
-    usuarios_obj = Usuarios.objects.get(id = id2)
-    incentivos = Incentivos.objects.get(id = id2)
-    return render(request, 'mis_incentivos.html', {'nombre':usuarios_obj.nombre, 'codigo':usuarios_obj.codigo, 'puntos':incentivos.puntos})
+    usuarios_obj = Usuario.objects.get(id_usuario = id2)
+    puntos = Puntos.objects.get(Usuario_id_usuario = id2)
+    return render(request, 'mis_incentivos.html', {'nombre':usuarios_obj.nombre, 'codigo':usuarios_obj.id_usuario, 'puntos':puntos.cantidad_puntos, 'fecha':puntos.fecha_puntos})
 
 def mi_actividad(request):
-    usuarios_obj = Usuarios.objects.get(id = id2)
-    incentivos = Incentivos.objects.get(id = id2)
-    return render(request, 'mi_actividad.html', {'nombre':usuarios_obj.nombre, 'codigo': usuarios_obj.codigo, 'cantidadRecogida':usuarios_obj.cantidadRecogida, 'puntos': incentivos.puntos})
+    usuarios_obj = Usuario.objects.get(id_usuario = id2)
+    puntos = Puntos.objects.get(Usuario_id_usuario = id2)
+    return render(request, 'mi_actividad.html', {'nombre':usuarios_obj.nombre, 'codigo': usuarios_obj.id_usuario,'puntos': puntos.cantidad_puntos})
 
 def filtrar_por(request):
     return render(request, 'filtrar_por.html')
