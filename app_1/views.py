84321df6467dc1cd0a44eac6fb75641406c86041
EsteTruji <etrujilloc@eafit.edu.co>
2022-10-16 03:48:44 -0500

    R25 | Se implementó la clasificación por formulario, y se implementó completamente la clasificación || Se refinaron algunos aspectos de usabilidad en los templates

--- app_1/views.py
+++ app_1/views.py
@@ -38,7 +38,7 @@ def clasificado_etiqueta(request):
     except:
         archivo = ""
         enlace_caneca = "https://rimoplasticas.com/wp-content/uploads/2020/12/colores-de-la-caneca-de-reciclaje.jpg"
-        return render(request, 'clasificado_etiqueta.html', {'archivo':archivo, 'enlace':enlace_caneca})
+        return render(request, 'clasificado_etiqueta.html', {'archivo':"...", 'enlace':enlace_caneca})
     else:
         caneca = ""
         enlace_caneca = ""
@@ -47,29 +47,66 @@ def clasificado_etiqueta(request):
         with open(os.path.join(ruta, archivo), 'r') as f:
             archivo2 = json.load(f)
         if archivo2['Capacity'] != "N/A":
-            capacidad = archivo2['Capacity'] + "ml"
+            capacidad = archivo2['Capacity'] + " ml"
         else:
             capacidad = archivo2['Capacity']
         if archivo2['Material'] == "Other plastic":
-            caneca = "Caneca negra"
+            caneca = "caneca negra (residuos no aprovechables)"
             enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
         elif archivo2['Dirtiness'] == "Clean" and archivo2['Damage'] == "Undamaged":
-            caneca = "Caneca blanca"
+            caneca = "caneca blanca (residuos aprovechables)"
             enlace_caneca = "https://i.postimg.cc/vBC5gj0Y/7ec7efb0-caneca-blanca-para-separacion-de-residuos-y-reciclaje-150x150-1.png"
         elif archivo2['Dirtiness'] != "Clean": 
-            caneca = "Caneca negra"
+            caneca = "caneca negra (residuos no aprovechables)"
             enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
         elif archivo2['Dirtiness'] == "Clean" and archivo2['Material'] != "Glass":
-            caneca = "Caneca blanca"
+            caneca = "caneca blanca (residuos aprovechables)" 
             enlace_caneca = "https://i.postimg.cc/vBC5gj0Y/7ec7efb0-caneca-blanca-para-separacion-de-residuos-y-reciclaje-150x150-1.png"
         elif archivo2['Damage'] != "Undamaged" and archivo2['Material'] == "Glass":
-            caneca = "Caneca negra"
+            caneca = "caneca negra (residuos no aprovechables)"
             enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
 
         return render(request, 'clasificado_etiqueta.html', {'archivo':caneca, 'enlace': enlace_caneca, 'Material': archivo2['Material'], 'Package_color': archivo2['Package_color'],'Bottle_cap': archivo2['Bottle_cap'],'Dirtiness': archivo2['Dirtiness'], 'Packaging_type': archivo2['Packaging_type'], 'Brand': archivo2['Brand'], 'Reference': archivo2['Reference'], 'Capacity': capacidad, 'Damage': archivo2['Damage']})
 
 def clasificado_formulario(request):
-    return render(request, 'clasificado_formulario.html')
+    try:
+        caneca = ""
+        enlace_caneca = ""
+        material = request.POST['material']
+        package_color = request.POST['package_color']
+        bottle_cap = request.POST['bottle_cap']
+        dirtiness = request.POST['dirtiness']
+        packaging_type = request.POST['packaging-type']
+        brand = request.POST['brand']
+        reference = request.POST['reference']
+        capacity = request.POST['capacity']
+        damage = request.POST['damage']
+        if capacity != "N/A":
+            capacidad = capacity + " ml"
+        else:
+            capacidad = capacity
+        if material == "Other plastic":
+            caneca = "caneca negra (residuos no aprovechables)"
+            enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
+        elif dirtiness == "Clean" and damage == "Undamaged":
+            caneca = "caneca blanca (residuos aprovechables)"
+            enlace_caneca = "https://i.postimg.cc/vBC5gj0Y/7ec7efb0-caneca-blanca-para-separacion-de-residuos-y-reciclaje-150x150-1.png"
+        elif dirtiness != "Clean": 
+            caneca = "caneca negra (residuos no aprovechables)"
+            enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
+        elif dirtiness == "Clean" and material != "Glass":
+            caneca = "caneca blanca (residuos aprovechables)"
+            enlace_caneca = "https://i.postimg.cc/vBC5gj0Y/7ec7efb0-caneca-blanca-para-separacion-de-residuos-y-reciclaje-150x150-1.png"
+        elif damage != "Undamaged" and material == "Glass":
+            caneca = "caneca negra (residuos no aprovechables)"
+            enlace_caneca = "https://i.postimg.cc/gjM4T4D1/137d264c-caneca-negra-para-separacion-de-residuos-y-reciclaje.png"
+    except:
+        archivo = ""
+        enlace_caneca = "https://rimoplasticas.com/wp-content/uploads/2020/12/colores-de-la-caneca-de-reciclaje.jpg"
+        return render(request, 'clasificado_formulario.html', {'archivo': "...",'enlace':enlace_caneca})
+    else:
+        return render(request, 'clasificado_formulario.html', {'archivo':caneca, 'enlace': enlace_caneca, 'Material': material, 'Package_color': package_color,'Bottle_cap': bottle_cap,'Dirtiness': dirtiness, 'Packaging_type': packaging_type, 'Brand': brand, 'Reference': reference, 'Capacity': capacidad, 'Damage': damage})
+        
 
 def etiquetaExito(request):
     usuarios_obj = Usuario.objects.get(id_usuario = id2)
