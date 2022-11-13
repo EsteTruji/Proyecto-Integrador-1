078f5f0baa81738b1445f9f320a637a76d22307a
EsteTruji <etrujilloc@eafit.edu.co>
2022-11-12 21:22:03 -0500

    Cambios en importaciones

--- EcoMind/urls.py
+++ EcoMind/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from asyncio.windows_events import NULL
 from django.contrib import admin
 from django.urls import path
 from app_1 import views as app1Views
